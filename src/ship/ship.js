8780c3b205a4fd31fc99e1ab9881b1ab2571a409
Shaun MacWilliam <shaun.macwilliam@gmail.com>
2023-11-17 22:44:03 +0000

    Ship class complete

--- src/ship/ship.js
+++ src/ship/ship.js
@@ -2,9 +2,15 @@ export default class Ship {
   constructor(length) {
     this.length = length;
     this.hits = 0;
+    this.isSunk = false;
   }
 
   hit() {
     this.hits++;
+    this.sink();
+  }
+
+  sink() {
+    if (this.hits >= this.length) this.isSunk = true;
   }
 }
--- src/ship/ship.test.js
+++ src/ship/ship.test.js
@@ -7,25 +7,65 @@ describe("Initialising a new Ship", () => {
     expect(ship).toBeInstanceOf(Ship);
     expect(ship).toHaveProperty("length", 5);
     expect(ship).toHaveProperty("hits", 0);
+    expect(ship).toHaveProperty("isSunk", false);
   });
 });
 
-describe("The hit() function", () => {
+describe("The hit function", () => {
   let ship;
 
   beforeEach(() => {
     ship = new Ship(5);
   });
 
-  test("One hit should increase the hits property by 1", () => {
-    // const ship = new Ship(5);
+  test("A single hit call should increase the hits property once", () => {
     ship.hit();
     expect(ship.hits).toEqual(1);
   });
 
-  test("Two hits should increase the hits property by 2", () => {
+  test("Multiple hit calls should increase the hits property by the number of calls", () => {
     ship.hit();
     ship.hit();
     expect(ship.hits).toEqual(2);
+  });
+});
+
+describe("The sink function", () => {
+  let ship;
+
+  beforeEach(() => {
+    ship = new Ship(4);
+    ship.hit();
+    ship.hit();
+  });
+
+  test("A ship with hits shouldn't be sunk", () => {
+    expect(ship.hits).toEqual(2);
+    expect(ship.length).toEqual(4);
+    expect(ship.isSunk).toBeFalsy;
+  });
+
+  test("A ship that recieves a hit shouldn't sink", () => {
+    ship.hit();
+    expect(ship.hits).toEqual(3);
+    expect(ship.length).toEqual(4);
+    expect(ship.isSunk).toBeFalsy;
+  });
+
+  test("A ship that recieves a hit that equals it's length should sink", () => {
+    ship.hit();
+    ship.hit();
+    expect(ship.hits).toEqual(4);
+    expect(ship.length).toEqual(4);
+    expect(ship.isSunk).toBeTruthy;
+  });
+
+  test("If a ship somehow receives more hits that it's length, it should still sink", () => {
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.hits).toEqual(5);
+    expect(ship.length).toEqual(4);
+    expect(ship.isSunk).toBeTruthy;
   });
 });
